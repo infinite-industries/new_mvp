cd75ade4deaa1e004c71d6161b010fa46a0d0967
dima <shifting.planes@gmail.com>
2017-03-10 15:20:02 -0500

    added data bindings on the front end and basic routing

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var dotenv = require('dotenv');
+dotenv.load();      //get configuration file from .env
+
+//collector donated money -- worked through stripe
+
+//collector contacted I.I for work availability
+
+//collector downloaded the work
+
+
+module.exports = router;
